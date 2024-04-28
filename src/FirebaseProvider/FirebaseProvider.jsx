673c333473ce7660c79e4df00744832038b92f6d
himelmia535 <himelmia535@gmail.com>
2024-04-28 09:48:39 +0600

    create user with email and password

--- src/FirebaseProvider/FirebaseProvider.jsx
+++ src/FirebaseProvider/FirebaseProvider.jsx
@@ -1,9 +1,30 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import auth from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null)
 const FirebaseProvider = ({children}) => {
 
-    const allValues = {name: "Test"}
+    const[user, setUser] = useState(null)
+
+    // create user
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+
+    // observer
+    useEffect(()=>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              setUser(user)
+            } 
+          });
+    },[])
+
+    const allValues = {
+        createUser
+    }
 
     return (
         <AuthContext.Provider value={allValues}>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,67 @@
+import { useContext } from "react";
+import { AuthContext } from "../FirebaseProvider/FirebaseProvider";
+import { useForm } from "react-hook-form"
+
 const Register = () => {
+  const {createUser} = useContext(AuthContext)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = (data) => {
+    const{email, password} = data
+    createUser(email, password)
+      .then(result => {
+        console.log(result);
+      })
+  };
+  
     return (
         <div>
-            <h2 className="text-3xl">This is Register</h2>
+            <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col ">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Register now!</h1>
+    </div>
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Name</span>
+          </label>
+          <input name="name" type="text" placeholder="Your Name" className="input input-bordered" {...register("fullName", { required: true })} />
+          {errors.fullName && <span className="text-red-500">This field is required</span>}
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input name="email" type="email" placeholder="email" className="input input-bordered" {...register("email", { required: true })} />
+          {errors.email && <span className="text-red-500">This field is required</span>}
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Image Url</span>
+          </label>
+          <input name="imageurl" type="text" placeholder="Image Url" className="input input-bordered" {...register("image")} />
+          
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input name="password" type="password" placeholder="password" className="input input-bordered" {...register("password", { required: true })} />
+          {errors.password && <span className="text-red-500">This field is required</span>}
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Register</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
