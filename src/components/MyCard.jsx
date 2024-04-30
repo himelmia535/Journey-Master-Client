229e8c4a5c6aa740ff3f09f37e34993908ab1c19
himelmia535 <himelmia535@gmail.com>
2024-04-30 18:32:20 +0600

    delete spot

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "animate.css";
-// import Swal from "sweetalert2";
+import Swal from "sweetalert2";
 
 const MyCard = ({ spot, spots, setspots }) => {
   const {
@@ -18,33 +18,33 @@ const MyCard = ({ spot, spots, setspots }) => {
     photo,
   } = spot;
 
-  //   const handleDelete = (_id) => {
-  //     console.log(_id);
-  //     Swal.fire({
-  //       title: "Are you sure?",
-  //       text: "You won't be able to revert this!",
-  //       icon: "warning",
-  //       showCancelButton: true,
-  //       confirmButtonColor: "#3085d6",
-  //       cancelButtonColor: "#d33",
-  //       confirmButtonText: "Yes, delete it!",
-  //     }).then((result) => {
-  //       if (result.isConfirmed) {
-  //         fetch(`http://localhost:5000/spot/${_id}`, {
-  //           method: "DELETE",
-  //         })
-  //           .then((res) => res.json())
-  //           .then((data) => {
-  //             console.log(data);
-  //             if (data.deletedCount > 0) {
-  //               Swal.fire("Deleted!", "Your spot has been deleted.", "success");
-  //               const remaining = spots.filter((cof) => cof._id !== _id);
-  //               setspots(remaining);
-  //             }
-  //           });
-  //       }
-  //     });
-  //   };
+    const handleDelete = (_id) => {
+      console.log(_id);
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:5000/touristsSpot/${_id}`, {
+            method: "DELETE",
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+              if (data.deletedCount > 0) {
+                Swal.fire("Deleted!", "Your spot has been deleted.", "success");
+                const remaining = spots.filter((cof) => cof._id !== _id);
+                setspots(remaining);
+              }
+            });
+        }
+      });
+    };
 
   return (
     <div className="card glass">
@@ -82,7 +82,7 @@ const MyCard = ({ spot, spots, setspots }) => {
               <button className="btn">Edit</button>
             </Link>
             <button
-              //   onClick={() => handleDelete(_id)}
+                onClick={() => handleDelete(_id)}
               className="btn bg-orange-500"
             >
               X
